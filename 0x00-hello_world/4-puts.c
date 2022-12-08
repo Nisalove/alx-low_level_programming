d52c39e1f188b016eaf28708dafaeb307909d2d4
Nisalove <anibenedictachinasa@gmail.com>
2022-12-08 08:34:51 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry
  *
- * Return: Always 0 (succes)
+ * Return: Always 0 (Succes)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (o);
+	return (0);
 }
